8a223dcda36ff67ea3a39321c0260c831a251bf0
duanzhonghuan <15818411038@163.com>
2019-04-01 23:31:02 +0800

    修改view-memory文件

---
+++ oslab/files/view-memory.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  view-memory.c
+ *
+ *    Description:  view the memory by debug
+ *
+ *        Version:  1.0
+ *        Created:  2019年03月29日 17时45分04秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned int i = 0x12345678;
+	printf("The logic or virtual address of i is 0x%x\n, &i");
+	while(1);
+	return 0;
+}
